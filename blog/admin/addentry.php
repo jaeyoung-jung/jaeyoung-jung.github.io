e97a55df7c797d345b49cb84909cdd759158fed5
Jaeyoung Jung <jungj@mit.edu>
2018-01-16 14:40:19 -0500

    Complete script

--- blog/admin/addentry.php
+++ blog/admin/addentry.php
@@ -1,5 +1,6 @@
 <?php
 	var_dump($_POST);
-	$blog_file=fopen("../blog_content.csv");
+	$blog_file=fopen("../blog_content.csv","a");
 	fputcsv($blog_file,array($_POST["title"],$_POST["author"],time(),$_POST["content"]));
+	fclose($blog_file);
 ?>
